eeedc035c3025f1dbf09d93efef25eed7b09fd31
silvio.filho <silvio.filho@tascominformatica.com.br>
2023-03-05 16:59:42 -0300

    #02 alert component and usage example

--- web-app/src/pages/Home/index.tsx
+++ web-app/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@mui/material";
+import useNotifier from "../../helpers/Notify";
 
 export default function Home() {
+    const notify = useNotifier()
     return <>
-        <Button variant="text" color="secondary">Text</Button>
-        <Button variant="contained" color="secondary">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+        <Button variant="text" color="secondary" onClick={() => notify('Informo que...', 'info')}>Text</Button>
+        <Button variant="contained" color="secondary" onClick={() => notify('Notificado', 'success')}>Contained</Button>
+        <Button variant="outlined" onClick={() => notify('errado', 'error')}>Outlined</Button>
     </>
 }
