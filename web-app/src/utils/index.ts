1f8e0b92a461af255c10da60a046c8783b59e091
ian.vinicius <ian.vinicius@tascomtecnologia.com.br>
2023-05-07 22:34:24 -0300

    hotfix: adding breakpoints and modify shadow on theme

---
+++ web-app/src/utils/index.ts
@@ -0,0 +1,3 @@
+import toBRL from "./toBRL";
+
+export { toBRL };
---
+++ web-app/src/utils/toBRL.ts
@@ -0,0 +1,7 @@
+export default function toBRL(value: number) {
+  return value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  });
+}
