72654d3f2bf7ea47bf76ba01201ca205088a509f
Amanda Gabriela <amandagabriela056@gmail.com>
2023-06-29 22:39:09 -0300

    Revert "#54-feat: add acquisitions route"
    This reverts commit 6fbbd16b06f4986063d1ad11c9f76246a6db644b.

--- web-app/src/configs/routes.tsx
+++ web-app/src/configs/routes.tsx
@@ -1,83 +1,71 @@
-import { lazy } from "react";
-import { RouteObject } from "react-router-dom";
-import Layout from "../components/Layout";
-import BooksExplore from "../pages/Books/Explore";
-import Acquisitions from "../pages/Books/Acquisitions";
-import NotFound from "../pages/NotFound";
-const Home = lazy(() => import("../pages/Home"));
-const BooksView = lazy(() => import("../pages/Books/View"));
-const Questions = lazy(() => import("../pages/Questions"));
-const SignUp = lazy(() => import("../pages/SignUp"));
-const SignIn = lazy(() => import("../pages/SignIn"));
-const ProfileSettings = lazy(() => import("../pages/Profile/Settings"));
-const Profile = lazy(() => import("../pages/Profile"));
-const RequireAuth = lazy(() => import("./RequireAuth"));
+    import { lazy } from 'react';
+    import { RouteObject } from 'react-router-dom';
+    import Layout from '../components/Layout';
+    import BooksExplore from '../pages/Books/Explore';
+    import NotFound from '../pages/NotFound';
+    const Home = lazy(() => import('../pages/Home'));
+    const BooksView = lazy(() => import('../pages/Books/View'));
+    const Questions = lazy(() => import('../pages/Questions'));
+    const SignUp = lazy(() => import('../pages/SignUp'));
+    const SignIn = lazy(() => import('../pages/SignIn'));
+    const ProfileSettings = lazy(() => import('../pages/Profile/Settings'));
+    const Profile = lazy(() => import('../pages/Profile'));
+    const RequireAuth = lazy(() => import('./RequireAuth'));
 
-const routes: RouteObject[] = [
-  {
-    path: "",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "login",
-        element: <SignIn />,
-      },
-      {
-        path: "register",
-        element: <SignUp />,
-      },
-      {
-        path: "profile",
-        children: [
-          {
-            index: true,
-            element: (
-              <RequireAuth>
-                <Profile />
-              </RequireAuth>
-            ),
-          },
-          {
-            path: "settings",
-            element: (
-              <RequireAuth>
-                <ProfileSettings />
-              </RequireAuth>
-            ),
-          },
-        ],
-      },
-      {
-        path: "explore",
-        children: [
-          {
-            index: true,
-            element: <BooksExplore />,
-          },
-          {
-            path: ":bookId",
-            element: <BooksView />,
-          },
-        ],
-      },
-      {
-        path: "questions",
-        element: <Questions />,
-      },
-      {
-        path: "acquisitions",
-        element: <Acquisitions />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-];
+    const routes: RouteObject[] = [
+        {
+            path: '',
+            element: <Layout />,
+            children: [
+                {
+                    index: true,
+                    element: <Home />
+                },
+                {
+                    path: 'login',
+                    element: <SignIn />
+                },
+                {
+                    path: 'register',
+                    element: <SignUp />
+                },
+                {
+                    path: 'profile',
+                    children: [
+                        {
+                            index: true,
+                            element: <RequireAuth><Profile /></RequireAuth>
+                        },
+                        {
+                            path: 'settings',
+                            element: <RequireAuth><ProfileSettings /></RequireAuth>
+                        }
+                    ]
 
-export default routes;
+                },
+                {
+                    path: 'explore',
+                    children: [
+                        {
+                            index: true,
+                            element: <BooksExplore />,
+                        },
+                        {
+                            path: ':bookId',
+                            element: <BooksView />,
+                        }
+                    ]
+                },
+                {
+                    path: 'questions',
+                    element: <Questions />
+                },
+                {
+                    path: '*',
+                    element: <NotFound />
+                }
+            ]
+        },
+    ]
+
+    export default routes;
