76eddb61e2a3d299c627d22a89fa177ff870cb7a
Amanda Gabriela <amandagabriela056@gmail.com>
2023-06-29 22:48:09 -0300

    #36-feat: add screens for acquisitions listing

--- web-app/src/pages/Books/Actions/types.ts
+++ web-app/src/pages/Books/Actions/types.ts
@@ -1,14 +1,18 @@
 import { ICategory } from "../../../commons/ICategory";
 
 export type BooksActionsProps = {
-    filter?: boolean,
-    publish?: boolean,
-    handleOpenPublish?: (v:true) => void,
-    handleFilter?: (v: any) => void
-}
+  filter?: boolean;
+  publish?: boolean;
+  handleOpenPublish?: (v: true) => void;
+  handleFilter?: (v: any) => void;
+  dateLabels?: {
+    minDate?: string;
+    maxDate?: string;
+  };
+};
 
 export type BooksFilters = {
-    min_date: string
-    max_date: string
-    categories: string[] 
-}
+  min_date: string;
+  max_date: string;
+  categories: string[];
+};
