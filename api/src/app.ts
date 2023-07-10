cd3a6f8b755e91859de8801f8f230d1b7ec61a3a
i4n-v <ianvinicioss@gmail.com>
2023-07-09 21:27:37 -0300

    chore: removing cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -10,12 +10,7 @@ async function initApp() {
   const app = express();
 
   app.use(express.json());
-  app.use(
-    cors({
-      origin: '*',
-      optionsSuccessStatus: 200,
-    })
-  );
+  // app.use(cors());
   app.use('/public', express.static('public'));
   swagger(app);
   routes(app);
