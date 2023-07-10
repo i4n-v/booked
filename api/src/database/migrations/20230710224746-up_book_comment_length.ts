a8476d13dfaa0423b57670bb802fc408edb69e36
i4n-v <ianvinicioss@gmail.com>
2023-07-10 20:09:06 -0300

    hotfix: upp comment length

---
+++ api/src/database/migrations/20230710224746-up_book_comment_length.ts
@@ -0,0 +1,25 @@
+import { Migration } from 'sequelize-cli';
+
+const migration: Migration = {
+  async up(queryInterface, sequelize) {
+    try {
+      await queryInterface.changeColumn('Comments', 'description', {
+        type: sequelize.STRING(2000),
+      });
+    } catch (error: any) {
+      console.log(error);
+    }
+  },
+
+  async down(queryInterface, sequelize) {
+    try {
+      await queryInterface.changeColumn('Comments', 'description', {
+        type: sequelize.STRING,
+      });
+    } catch (error: any) {
+      console.log(error);
+    }
+  },
+};
+
+module.exports = migration;
