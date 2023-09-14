a574b6486f0ed23292513160b4994a994c9a7aab
i4n-v <ianvinicioss@gmail.com>
2023-09-14 08:22:50 -0300

    #45-feat: change dtos

--- api/src/dto/message/message.dto.ts
+++ api/src/dto/message/message.dto.ts
@@ -1,6 +1,10 @@
 export default class MessageDto {
   id: string;
+  chat_id: string;
   sender_id: string;
   receiver_id: string;
+  read: boolean;
   content: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
