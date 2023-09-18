4732fdbf60355c73fc64ceebc6420993ede066b1
i4n-v <ianvinicioss@gmail.com>
2023-09-18 15:27:03 -0300

    hotfix: adding createdAt and updatedAt to migrations

--- api/src/database/migrations/20230323222208-create-authentication-table.ts
+++ api/src/database/migrations/20230323222208-create-authentication-table.ts
@@ -32,6 +32,16 @@ const migration: Migration = {
           },
           allowNull: false,
         },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('NOW'),
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('NOW'),
+        },
       });
     } catch (error: any) {
       console.log(error);
