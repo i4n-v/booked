34ba6ff693c270635476c0f5292576fc03a2caef
Suelen Salvino <suelensalvinocs@gmail.com>
2023-10-03 20:30:49 -0300

    #43-feat: adding file input story

---
+++ web-app/src/stories/File/index.stories.tsx
@@ -0,0 +1,71 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import File from "../../components/Input/File";
+import { InputFileProps } from "../../components/Input/File/types";
+import { FormProvider, useForm } from "react-hook-form";
+import FormWrapperProps from "./types";
+import {
+    Description,
+    Subtitle,
+    Title,
+    Primary,
+    Controls,
+  } from "@storybook/blocks";
+
+const  meta: Meta<InputFileProps> = {
+    component: File,
+    tags: ["autodocs"],
+    parameters: {
+        docs: {
+            page: () => (
+                <>
+                <Title />
+                <Description>File input component allows users to attach one or multiple files.</Description>
+                <Subtitle>See the examples and put them into practice:</Subtitle>
+                <Primary />
+                <Controls />
+              </>
+            ),
+        },
+    },
+    argTypes: {
+        name: {
+            description: "Specifies the name of an element."
+        },
+        button: {
+            description: "Button for file uploads."
+        },
+        accept: {
+            description: "The accept attribute specifies a filter for what file types the user can pick from the file input dialog box."
+        },
+        label: {
+            description: "Represents a legend for an item."
+        },
+    },
+    args: {
+        name: "File",
+        button: true,
+        accept: "image/*",
+        label: "Label of Input"
+    },
+};
+
+export default meta;
+type Story = StoryObj<InputFileProps>;
+
+const FormWrapper = ({ children }: FormWrapperProps) => {
+    const methods = useForm();
+    return <FormProvider {...methods}>{children}</FormProvider>;
+  };
+
+export const Default: Story = {
+    render: ({ name, button, accept, label }: InputFileProps) => (
+      <FormWrapper>
+        <File 
+        name={name}
+        button={button}
+        accept={accept}
+        label={label}
+        />
+      </FormWrapper>
+    ),
+};
---
+++ web-app/src/stories/File/types.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+interface FormWrapperProps {
+  children: ReactNode;
+}
+
+export default FormWrapperProps;
