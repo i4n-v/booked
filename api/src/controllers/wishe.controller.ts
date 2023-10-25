01e828ad8ea216a9972500ca8f9180f4c1083da2
Amanda Gabriela <amandagabriela056@gmail.com>
2023-10-25 16:00:59 -0300

    #48-feat: adding routes to list whises and list wishes books

--- api/src/controllers/wishe.controller.ts
+++ api/src/controllers/wishe.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import BookRepository from '../repositories/book.repository';
 import WisheRepository from '../repositories/wishe.repository';
 import messages from '../config/messages.config';
+import paginationWrapper from '../utils/paginationWrapper';
 
 class WisheController {
   async store(request: Request, response: Response, next: NextFunction) {
@@ -22,6 +23,29 @@ class WisheController {
 
       return response.json({ message: 'Livro favoritado com sucesso!' });
     } catch (error) {
+      next(error);
+    }
+  }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      // Desestruturação dos objetos de request
+      const { auth, query } = request;
+
+      // Parse dos parâmetros da URL para paginação
+      const page = query.page ? parseInt(query.page as unknown as string) : 1;
+      const limit = query.limit ? parseInt(query.limit as unknown as string) : 75;
+
+      // Consulta ao banco de dados para listar todos os livros
+      const books = await BookRepository.findAndCountAll(page, limit, request, {});
+
+      // Paginação dos resultados
+      const wrappedBooks = paginationWrapper(books, page, limit);
+
+      // Retorna os resultados em formato JSON
+      return response.json(wrappedBooks);
+    } catch (error) {
+      // Em caso de erro, chama a próxima função de middleware de tratamento de erros (Express.js)
       next(error);
     }
   }
--- api/src/routes/whise.routes.ts
+++ api/src/routes/whise.routes.ts
@@ -4,12 +4,75 @@ import authMidleware from '../midlewares/auth.midleware';
 
 /**
  * @openapi
+ * /wishe/books:
+ *   get:
+ *     summary: List wishes books.
+ *     description: This route list wishes books with pagination.
+ *     tags:
+ *       - Wishe
+ *     parameters:
+ *       - $ref: '#/components/parameters/access_token'
+ *       - $ref: '#/components/parameters/page'
+ *       - $ref: '#/components/parameters/limit'
+ *     security:
+ *       - access_token: []
+ *     responses:
+ *       200:
+ *         description: Return the list of acquired books.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               id: string
+ *               name: string
+ *               description: string
+ *               price: float
+ *               photo_url: string
+ *               user_id: string
+ *               createdAt: string
+ *               updatedAt: string
+ *               rating: float
+ *               total_users_rating: integer
+ *               categories: array
+ *             examples:
+ *               get_payload:
+ *                 value:
+ *                   - id: 'ebd486ce-8dbd-4e13-bf46-e03c3fa5f3dc'
+ *                     name: 'Mundo mágico'
+ *                     description: 'Um grande mundo mágico.'
+ *                     price: 12.56
+ *                     photo_url: 'http://localhost:5000/public/uploads/images/1f96b637e1b9b174ffc4d3030e87b71c-example-image.png'
+ *                     user_id:
+ *                         id: 'a99fb524-6fea-4dc1-a8f0-66410097266b'
+ *                         name: 'amanda'
+ *                         user_name: 'amanda#0'
+ *                     createdAt: '2023-05-15T01:48:16.006Z'
+ *                     updatedAt: '2023-05-15T01:48:16.006Z'
+ *                     rating: 0
+ *                     total_users_rating: 0
+ *                     categories:
+ *                       - id: 'a99fb524-bfea-4dc1-a8f0-66410097266b'
+ *                         name: 'Ação'
+ *                       - id: 'a29fb524-6fea-4dc1-a8f0-66410097266b'
+ *                         name: 'Aventura'
+ *
+ *
+ *
+ *       400:
+ *         $ref: '#/components/responses/error'
+ *       401:
+ *         $ref: '#/components/responses/error'
+ */
+router.get('/wishe/books', authMidleware, WisheController.index);
+
+/**
+ * @openapi
  * /wishe/books/{id}:
  *   post:
  *     summary: Create a whise.
  *     description: This route create a book's whise to a user.
  *     tags:
- *       - Whise
+ *       - Wishe
  *     parameters:
  *       - name:
  *         $ref: '#/components/parameters/access_token'
