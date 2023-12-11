aab846409345098b4b92d7d1b286ad4f691435d2
Amanda Gabriela <amandagabriela056@gmail.com>
2023-12-11 14:22:55 -0300

    #51-feat: create model and migration of followers users

---
+++ api/src/database/migrations/20232023298345-create_follower_table.ts
@@ -0,0 +1,53 @@
+import { Migration } from 'sequelize-cli';
+
+const migration: Migration = {
+  async up(queryInterface, Sequelize) {
+    try {
+      await queryInterface.createTable('Followers', {
+        id: {
+          type: Sequelize.DataTypes.UUID,
+          defaultValue: Sequelize.DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        user_id: {
+          type: Sequelize.DataTypes.UUID,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          allowNull: false,
+        },
+        followed_id: {
+          type: Sequelize.DataTypes.UUID,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          allowNull: false,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('NOW'),
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.fn('NOW'),
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async down(queryInterface) {
+    try {
+      await queryInterface.dropTable('Followers');
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+module.exports = migration;
---
+++ api/src/database/models/follower.model.ts
@@ -0,0 +1,31 @@
+import {
+  AllowNull,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+import User from './user.model';
+import FollowerCreateDto from '../../dto/follower/followerCreate.dto';
+
+@Table
+export default class Follower extends Model<FollowerCreateDto> {
+  @PrimaryKey
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  id: string;
+
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  @ForeignKey(() => User)
+  user_id: string;
+
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  @ForeignKey(() => User)
+  followed_id: string;
+}
