e192dafe7a2854e8d939930af692308c80efa393
Amanda Gabriela <amandagabriela056@gmail.com>
2024-08-31 18:19:50 -0300

    feat: creating solicitation screen to app

--- mobile-app/src/app/(app)/(tabs)/solicitations/index.tsx
+++ mobile-app/src/app/(app)/(tabs)/solicitations/index.tsx
@@ -1,11 +1,58 @@
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { SolicitationCard } from "@/components/Cards";
+import { Text } from "react-native-svg";
+import { useSolicitation } from "@/services";
+import { ListCounter } from "@/components";
 
-function FirstExample() {
+export default function Solicitations() {
+  const { getSolicitations } = useSolicitation();
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [totalPages, setTotalPages] = useState<number>(1);
+
+  useEffect(() => {
+    const fetchSolicitations = async () => {
+      setLoading(true);
+      try {
+        const response = await getSolicitations({});
+        setData(response.items);
+        setTotalPages(response.totalPages);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchSolicitations();
+  }, []);
+
+  if (loading) {
+    return <Text>Carregando...</Text>;
+  }
+
+  if (error) {
+    return <Text>Erro: {error}</Text>;
+  }
+
   return (
-    <View>
-      <Text>Solicitations screen</Text>
-    </View>
+    <>
+      <ListCounter title="Solicitações" count={data.length} total={totalPages} />
+      <SafeAreaView>
+        <ScrollView>
+          {data.map((item) => (
+            <SolicitationCard
+              key={item.id}
+              id={item.id}
+              status={item.status}
+              book={item.book}
+              user={item.user}
+            />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
-
-export default FirstExample;
